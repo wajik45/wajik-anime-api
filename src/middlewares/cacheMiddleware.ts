bf096bcefd456a62f2a6281de4c827871880d609
wajik45 <haikal2589780@gmail.com>
2024-10-14 13:05:28 +0700

    sip

--- src/middlewares/cacheMiddleware.ts
+++ src/middlewares/cacheMiddleware.ts
@@ -3,34 +3,34 @@ import { defaultTTL, getFromCache } from "../helpers/cache";
 import { responseJSON, type PayloadProps } from "../helpers/responses";
 import getMinFromMs from "../helpers/getMinFromMs";
 import path from "path";
-import fs from "fs";
+// import fs from "fs";
 
-function writeLog(message: string, cache: "hit" | "miss") {
-  function wajibDua(data: any) {
-    return `${data}`.length === 1 ? `0${data}` : `${data}`;
-  }
+// function writeLog(message: string, cache: "hit" | "miss") {
+//   function wajibDua(data: any) {
+//     return `${data}`.length === 1 ? `0${data}` : `${data}`;
+//   }
 
-  const fileName = "logs.txt";
-  const filePath = path.join(__dirname, "..", "..", "public", fileName);
-  const d = new Date();
-  const date = wajibDua(d.getDate());
-  const month = wajibDua(d.getMonth());
-  const year = d.getFullYear();
-  const hours = wajibDua(d.getHours());
-  const minutes = wajibDua(d.getMinutes());
-  const seconds = wajibDua(d.getSeconds());
-  const logMessage = `[${date}-${month}-${year}] [${hours}:${minutes}:${seconds}] [${cache}] [${message}]\n`;
+//   const fileName = "logs.txt";
+//   const filePath = path.join(__dirname, "..", "..", "public", fileName);
+//   const d = new Date();
+//   const date = wajibDua(d.getDate());
+//   const month = wajibDua(d.getMonth());
+//   const year = d.getFullYear();
+//   const hours = wajibDua(d.getHours());
+//   const minutes = wajibDua(d.getMinutes());
+//   const seconds = wajibDua(d.getSeconds());
+//   const logMessage = `[${date}-${month}-${year}] [${hours}:${minutes}:${seconds}] [${cache}] [${message}]\n`;
 
-  if (!fs.existsSync(filePath)) {
-    fs.writeFile(filePath, logMessage, (err) => {
-      if (err) throw err;
-    });
-  } else {
-    fs.appendFile(filePath, logMessage, (err) => {
-      if (err) throw err;
-    });
-  }
-}
+//   if (!fs.existsSync(filePath)) {
+//     fs.writeFile(filePath, logMessage, (err) => {
+//       if (err) throw err;
+//     });
+//   } else {
+//     fs.appendFile(filePath, logMessage, (err) => {
+//       if (err) throw err;
+//     });
+//   }
+// }
 
 /**
  * @param ttl minutes
@@ -46,16 +46,16 @@ export function cacheMiddleware(ttl?: number) {
     const cachedData: PayloadProps = getFromCache(key);
 
     if (cachedData) {
-      console.log("hit");
-      writeLog(key, "hit");
+      // console.log("hit");
+      // writeLog(key, "hit");
 
       const { data, pagination, message } = cachedData;
 
       return responseJSON.ok(req, res, { data, pagination, message });
     }
 
-    console.log("miss");
-    writeLog(key, "miss");
+    // console.log("miss");
+    // writeLog(key, "miss");
 
     next();
   };
